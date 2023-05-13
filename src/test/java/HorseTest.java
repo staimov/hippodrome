05c4674117320db5eed9d69cb4ef8aaff23e1585
staimov <staimov@mail.ru>
2023-05-13 11:26:25 +0500

    HorseTest class updated

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,11 +1,16 @@
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class HorseTest {
     @Test
+    @Order(10)
     void constructorWithNullFirstParamShouldThrowIllegalArgumentException() {
         assertThrows(
                 IllegalArgumentException.class,
@@ -13,6 +18,7 @@ class HorseTest {
     }
 
     @Test
+    @Order(20)
     void constructorWithNullFirstParamShouldThrowExceptionWithRelevantMessage() {
         String message = null;
         try {
@@ -26,6 +32,7 @@ class HorseTest {
     }
 
     @ParameterizedTest
+    @Order(30)
     @ValueSource(strings = { "", " ", "  ", "\t", "\t\t", " \t" })
     void constructorWithBlankFirstParamShouldThrowIllegalArgumentException(String str) {
         assertThrows(
@@ -34,6 +41,7 @@ class HorseTest {
     }
 
     @ParameterizedTest
+    @Order(40)
     @ValueSource(strings = { "", " ", "  ", "\t", "\t\t", " \t" })
     void constructorWithBlankFirstParamShouldThrowExceptionWithRelevantMessage(String str) {
         String message = null;
@@ -48,6 +56,7 @@ class HorseTest {
     }
 
     @Test
+    @Order(50)
     void constructorWithNegativeSecondParamShouldThrowIllegalArgumentException() {
         assertThrows(
                 IllegalArgumentException.class,
@@ -55,6 +64,7 @@ class HorseTest {
     }
 
     @Test
+    @Order(60)
     void constructorWithNegativeSecondParamShouldThrowExceptionWithRelevantMessage() {
         String message = null;
         try {
@@ -68,6 +78,7 @@ class HorseTest {
     }
 
     @Test
+    @Order(70)
     void constructorWithNegativeThirdParamShouldThrowIllegalArgumentException() {
         assertThrows(
                 IllegalArgumentException.class,
@@ -75,6 +86,7 @@ class HorseTest {
     }
 
     @Test
+    @Order(80)
     void constructorWithNegativeThirdParamShouldThrowExceptionWithRelevantMessage() {
         String message = null;
         try {
@@ -86,4 +98,44 @@ class HorseTest {
 
         assertEquals("Distance cannot be negative.", message);
     }
+
+    @Test
+    @Order(90)
+    void getNameShouldReturnConstructorFirstParamValue() {
+        String expected = "foo";
+        Horse horse = new Horse(expected, 0.0, 0.0);
+        String actual = horse.getName();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Order(100)
+    void getSpeedShouldReturnConstructorSecondParamValue() {
+        double expected = 2.73;
+        Horse horse = new Horse("foo", expected, 0.0);
+        double actual = horse.getSpeed();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Order(110)
+    void getDistanceShouldReturnConstructorThirdParamValue() {
+        double expected = 2.73;
+        Horse horse = new Horse("foo", 0.0, expected);
+        double actual = horse.getDistance();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Order(120)
+    void getDistanceShouldReturnZeroIfTwoParamConstructorUsed() {
+        Horse horse = new Horse("foo", 0.0);
+        double actual = horse.getDistance();
+
+        assertEquals(0.0, actual);
+    }
+
 }
