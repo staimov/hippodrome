f8060ea0bca637de102b21417df7e3f03c2271f0
staimov <staimov@mail.ru>
2023-05-13 12:26:07 +0500

    MainTest class and main test added

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    @Timeout(value = 22, unit = TimeUnit.SECONDS)
+    @Disabled
+    void mainShouldRunNotLongerThanTimeout() throws Exception {
+        Main.main(new String[0]);
+    }
+}
